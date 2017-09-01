8f6f695e1bc4624efd691c887a1f203dbbca080c
gnylytsya <gnylytsya@gmail.com>
2017-09-01 12:17:42 +0300

    HW JS Essentials 1

---
+++ script.js
@@ -0,0 +1,3 @@
+var dollar = '25.58', userDollar = prompt('Введіть суму доларів', '');
+var euro = '30.46', userEuro = prompt('Введість суму євро', '');
+alert( userEuro + ' euros are equal ' + euro * userEuro + ' grns, ' + userDollar + ' dollars are equal ' + dollar * userDollar + ', ' + ' one euro is equal 1.18 dollars. ');
