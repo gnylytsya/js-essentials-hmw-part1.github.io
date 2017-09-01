608ffc35af1656ab976f96ef3a0564c4a1fdfb76
gnylytsya <gnylytsya@gmail.com>
2017-09-01 12:25:57 +0300

    add 2 parts HW JS

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-var dollar = '25.58', userDollar = prompt('Введіть суму доларів', '');
-var euro = '30.46', userEuro = prompt('Введість суму євро', '');
-alert( userEuro + ' euros are equal ' + euro * userEuro + ' grns, ' + userDollar + ' dollars are equal ' + dollar * userDollar + ', ' + ' one euro is equal 1.18 dollars. ');
+var name = 'Jon', time = '17:00', place = 'Lviv', event = prompt('Введіть подію', '');
+alert( name + ' has a ' + event + ' today at ' + time + ' somewhere in ' + place );
